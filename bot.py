6938c3571b6d9b46faf00a890a1e10752cb8656d
IllyaKol777 <133062066+IllyaKol777@users.noreply.github.com>
2025-07-05 23:25:21 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from database import init_db
+from handlers import start, menu, shop, cart, order
+
+"""
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    init_db()
+
+    dp.include_routers(
+        start.router,
+        menu.router,
+        shop.router,
+        cart.router,
+        order.router
+    )
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+"""
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+init_db()
+
+dp.include_routers(
+    start.router,
+    menu.router,
+    shop.router,
+    cart.router,
+    order.router
+)
